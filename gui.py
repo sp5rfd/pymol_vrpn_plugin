0b418f6a829f75b4488b38a06db151d3fc4ee4e9
Paweł Tomaszewski <pawtom@dworkowa.imdik.pan.pl>
2017-01-30 15:18:26 +0100

    gui doskonalenie

--- gui.py
+++ gui.py
@@ -9,56 +9,88 @@ Witaj użytkowniku.\
 \ncząsteczek chemicznych takich jak białka czy kwasy nukleinowe\
 \nlklklk"
 
+currentWindow=0
+
 def filesWindow():
-    window = Tk()
-    window.title("Eksplorator lokalnych podobieństw struktur przestrzennych")
+    global currentWindow
+    currentWindow = Tk()
+    currentWindow.title("Eksplorator lokalnych podobieństw struktur przestrzennych")
 
     w=640
     h=480
-    x=window.winfo_screenwidth()/2 - w/2
-    y=window.winfo_screenheight()/2 - h/2
-    window.geometry("%dx%d+%d+%d" % (w,h,x,y))
+    x=currentWindow.winfo_screenwidth()/2 - w/2
+    y=currentWindow.winfo_screenheight()/2 - h/2
+    currentWindow.geometry("%dx%d+%d+%d" % (w,h,x,y))
     
-    group=LabelFrame(window,text="Witaj",padx=5,pady=5)
-    group.pack(fill=BOTH,padx=5,pady=5,expand=True)
+    group=LabelFrame(currentWindow,text="Wzorzec",padx=5,pady=5)
+    group.pack(fill=BOTH,padx=5,pady=5)
     
-    label=Label(group,text=helloMsg,bg="green")
-    label.pack(fill=BOTH,side=LEFT)
+    message="Wzorzec jest strukturą, którą będziemy próbowali dopasować do cząsteczki bazowej."
     
-    dnaImage=PhotoImage(file="dna.gif")
-    
-    label=Label(group,image=dnaImage)
+    label=Label(group,text=message,bg="green",anchor=W)
     label.pack(fill=BOTH)
     
-    group=Frame(window)
-    group.pack(padx=5)
+    fileName=Entry(group)
+    fileName.pack(pady=5,side=LEFT)
     
-    closeButton=Button(group,text="Anuluj",command=closeAction)
+    fileChooser=Button(group,text="Wybierz plik")
+    fileChooser.pack(side=LEFT)
+    
+    group=LabelFrame(currentWindow,text="Plik mapowania",padx=5,pady=5)
+    group.pack(fill=BOTH,padx=5,pady=5)
+    
+    label=Label(group,text="Tutaj wybierz plik mapowania",bg="green",anchor=W)
+    label.pack(fill=BOTH)
+    
+    fileName=Entry(group)
+    fileName.pack(pady=5,side=LEFT)
+    
+    fileChooser=Button(group,text="Wybierz plik")
+    fileChooser.pack(side=LEFT)
+    
+    group=LabelFrame(currentWindow,text="Plik bazowy",padx=5,pady=5)
+    group.pack(fill=BOTH,padx=5,pady=5)
+    
+    label=Label(group,text="Tutaj wybierz plik ",bg="green",anchor=W)
+    label.pack(fill=BOTH)
+        
+    fileName=Entry(group)
+    fileName.pack(pady=5,side=LEFT)
+    
+    fileChooser=Button(group,text="Wybierz plik")
+    fileChooser.pack(side=LEFT)
+    
+    group=Frame(currentWindow)
+    group.pack(padx=5,expand=TRUE)
+    
+    group=Frame(currentWindow)
+    group.pack(padx=5,pady=5)
+    
+    closeButton=Button(group,text="Anuluj",command=sys.exit)
     closeButton.pack(side=RIGHT)
     
-    nextButton=Button(group,text="Dalej",command=nextAction)
+    nextButton=Button(group,text="Dalej")
     nextButton.pack()
     
-    window.mainloop();
-
-def closeAction():
-    sys.exit()
+    currentWindow.mainloop();
     
-def nextAction(prevWindow):
-    prevWindow.close()
+def nextWindow():
+    global currentWindow
+    currentWindow.destroy()
     filesWindow()
     
 def startWindow():
-    window = Tk()
-    window.title("Eksplorator lokalnych podobieństw struktur przestrzennych")
+    global currentWindow
+    currentWindow = Tk()
+    currentWindow.title("Eksplorator lokalnych podobieństw struktur przestrzennych")
 
     w=640
     h=480
-    x=window.winfo_screenwidth()/2 - w/2
-    y=window.winfo_screenheight()/2 - h/2
-    window.geometry("%dx%d+%d+%d" % (w,h,x,y))
+    x=currentWindow.winfo_screenwidth()/2 - w/2
+    y=currentWindow.winfo_screenheight()/2 - h/2
+    currentWindow.geometry("%dx%d+%d+%d" % (w,h,x,y))
     
-    group=LabelFrame(window,text="Witaj",padx=5,pady=5)
+    group=LabelFrame(currentWindow,text="Witaj",padx=5,pady=5)
     group.pack(fill=BOTH,padx=5,pady=5,expand=True)
     
     label=Label(group,text=helloMsg,bg="green")
@@ -69,19 +101,16 @@ def startWindow():
     label=Label(group,image=dnaImage)
     label.pack(fill=BOTH)
     
-    group=Frame(window)
-    group.pack(padx=5)
+    group=Frame(currentWindow)
+    group.pack(padx=5,pady=5)
     
-    closeButton=Button(group,text="Anuluj",command=closeAction)
+    closeButton=Button(group,text="Anuluj",command=sys.exit)
     closeButton.pack(side=RIGHT)
     
-    nextButton=Button(group,text="Dalej",command=nextAction(window))
+    nextButton=Button(group,text="Dalej",command=nextWindow)
     nextButton.pack()
-
     
-    window.mainloop();
-
-
+    currentWindow.mainloop();
 
 if __name__ == "__main__":
     startWindow()
