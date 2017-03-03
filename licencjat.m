181a6de4d2027d347aaad0def7a775db0038ea44
Paweł Tomaszewski <ja@paweltomaszewski.pl>
2017-03-03 15:01:53 +0100

    mod

--- licencjat.m
+++ licencjat.m
@@ -1,3 +1,9 @@
+% wczytanie pliku mapowania i struktur pdb
 structure=pdbread('1fg0.pdb');
-mapping=fread('1fg0_helix.txt');
 template=pdbread('helix.pdb');
+mappingCells=textscan(fopen('1fg0_helix.txt'),'%s %s');
+mappingKey=cell2mat(mappingCells{2});
+mappingValue=str2num(cell2mat(mappingCells{1}));
+
+
+
